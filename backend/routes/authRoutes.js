4f43c75252207f62d931a027908a770c811e076c
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2025-10-29 17:20:50 +0530

    7

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,26 @@
+// import express from "express";
+// import { signup, login, logout, profile, makeAdmin, forgotPassword, resetPassword } from "../controllers/authController.js";
+// import { protect, isAdmin } from "../middleware/authMiddleware.js";
+
+// const router = express.Router();
+
+// router.post("/signup", signup);
+// router.post("/login", login);
+// router.post("/logout", logout);
+// router.get("/profile", protect, profile);
+// router.put("/make-admin", protect, isAdmin, makeAdmin);
+// router.post("/forgot-password", forgotPassword);
+// router.post("/reset-password/:token", resetPassword);
+
+// export default router;
+
+
 import express from "express";
-import { signup, login, logout, profile, makeAdmin, forgotPassword, resetPassword } from "../controllers/authController.js";
+import {
+  signup, login, logout, profile, makeAdmin,
+  forgotPassword, resetPassword,
+  getAllUsers, createUser, updateUser, deleteUser
+} from "../controllers/authController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -11,5 +32,9 @@ router.get("/profile", protect, profile);
 router.put("/make-admin", protect, isAdmin, makeAdmin);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
+router.get("/users", protect, isAdmin, getAllUsers);            // READ
+router.post("/users", protect, isAdmin, createUser);          // CREATE
+router.put("/users/:id", protect, isAdmin, updateUser);       // UPDATE
+router.delete("/users/:id", protect, isAdmin, deleteUser);    // DELETE
 
 export default router;
