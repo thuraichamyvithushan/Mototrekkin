5e03e5f4f7536fe667cdc4aa9c9b6500e0aa0480
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2025-10-29 15:57:30 +0530

    frintend url added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -111,7 +111,7 @@ export const forgotPassword = async (req, res) => {
     user.resetPasswordExpire = Date.now() + 60 * 60 * 1000;
     await user.save();
 
-    const resetUrl = `${process.env.FRONTEND_URL || "http://localhost:5174"}/reset-password/${resetToken}`;
+    const resetUrl = `${"https://mototrekkin-djyk.vercel.app" || "http://localhost:5174"}/reset-password/${resetToken}`;
 
     // Send email 
     const transporter = nodemailer.createTransport({
